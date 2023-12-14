2bc028135e7daca45948de00ba3dc84c0f569e3e
muhihsan <ihsan64.mi@gmail.com>
2023-12-14 11:14:59 +0700

    [FIX] fixing path in AppPath and authcontroller

--- src/main/java/com/engima/shopeymart/controller/AuthController.java
+++ src/main/java/com/engima/shopeymart/controller/AuthController.java
@@ -2,10 +2,15 @@ package com.engima.shopeymart.controller;
 
 import com.engima.shopeymart.constant.AppPath;
 import com.engima.shopeymart.dto.request.AuthRequest;
+import com.engima.shopeymart.dto.request.ProductRequest;
+import com.engima.shopeymart.dto.response.CommonResponse;
 import com.engima.shopeymart.dto.response.LoginResponse;
+import com.engima.shopeymart.dto.response.ProductResponse;
 import com.engima.shopeymart.dto.response.RegisterResponse;
 import com.engima.shopeymart.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,4 +33,18 @@ public class AuthController {
     public LoginResponse loginCustomer(@RequestBody AuthRequest authRequest){
         return authService.login(authRequest);
     }
+
+    //response entity digunakan untuk membuat return status code, message, dan kembalian datanya
+//    @PostMapping("/login")
+//    public ResponseEntity<?> loginCustomer(@RequestBody AuthRequest authRequest){
+//        LoginResponse loginResponse = authService.login(authRequest);
+//        return ResponseEntity.status(HttpStatus.ACCEPTED)
+//                .body(CommonResponse.<LoginResponse>builder()
+//                        .statusCode(HttpStatus.OK.value())
+//                        .message("Successfully created new product")
+//                        .data(loginResponse)
+//                        .build());
+//    }
+
+
 }
